5ca571a21491b9faaf3d47bcf0437afda4c5cad6
wangyangong <1176360298@qq.com>
2019-07-20 14:13:55 +0800

    平台的登录启动类

--- src/main/java/com/hanma/hanmaterraceclientserver/HanmaTerraceClientServerApplication.java
+++ src/main/java/com/hanma/hanmaterraceclientserver/HanmaTerraceClientServerApplication.java
@@ -2,12 +2,10 @@ package com.hanma.hanmaterraceclientserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
-@SpringBootApplication()
+@SpringBootApplication
 public class HanmaTerraceClientServerApplication {
 
     public static void main(String[] args) {
